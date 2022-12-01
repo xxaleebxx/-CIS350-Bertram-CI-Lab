50d80bf51528d8146548a4ba83521dc06a4a9e89
Autumn L. Bertram <bertrama@eos30.cis.gvsu.edu>
2022-12-01 16:44:36 -0500

    Added modulo division and exponents

--- math_functions.py
+++ math_functions.py
@@ -11,6 +11,12 @@ def multiply_numbers(a, b):
 def divide_numbers(a , b):
     return a / b
 
+def modulo_numbers(a, b):
+    return a % b
+
+def exponent_numbers(a, b):
+    return a**b
+
 if __name__ == "__main__":
     print ("Adding:", add_numbers(2,4))
     print("Subtracting:", subtract_numbers(9,2))
--- test_math_fuctions.py
+++ test_math_fuctions.py
@@ -20,3 +20,10 @@ def test_calc_divide():
     output = divide_numbers(10,2)
     assert output == 5
 
+def test_calc_modulo():
+    output = modulo_numbers(8,6)
+    assert output == 2
+
+def test_calc_exponent():
+    output = exponent_numbers(2,3)
+    assert output == 8
